79aca2f6c7114b44daa50b843da34ff7ac5ac942
Jonathan Anzules <jonanzule@gmail.com>
2024-02-26 17:42:37 -0800

    Update AUC_Test_simulations.R

--- Stats plots and data management/AUC_Test_simulations.R
+++ Stats plots and data management/AUC_Test_simulations.R
@@ -1,16 +1,10 @@
+library(pracma)
+
+#####
 # Data
 
 ModeldataWT = read.csv("C:/Laptop Backups/HomestaticExpansionProject/Code/Stats plots and data management/ModelOutputWT2.csv")
 ModeldataKO = read.csv("C:/Laptop Backups/HomestaticExpansionProject/Code/Stats plots and data management/ModelOutputKO2.csv")
-WTData = read.csv("C:/Laptop Backups/HomestaticExpansionProject/Code/Modeling/Matlab/RawData/ActivatedWTSpleen.csv")
-ProlWTData = read.csv("C:/Laptop Backups/HomestaticExpansionProject/Code/Modeling/Matlab/RawData/WTProl.csv")
-KOData = read.csv("C:/Laptop Backups/HomestaticExpansionProject/Code/Modeling/Matlab/RawData/ActivatedKOSpleen.csv")
-ProlKOData = read.csv("C:/Laptop Backups/HomestaticExpansionProject/Code/Modeling/Matlab/RawData/KOProl.csv")
-
-WTData$hours = WTData$hours / 24
-KOData$hours = KOData$hours / 24
-ProlWTData$hours = ProlWTData$hours / 24
-ProlKOData$hours = ProlKOData$hours / 24
 
 colnames(ModeldataWT) = c("NaiveCT", "ActTCT", "TregCT", "ThyNaive", "ActTNaive", "ThyTregs",
                           "TregNaive", "ProlNaive", "ProlActT", "ProlTreg", "IL-2", "ThymWeigth")
@@ -22,3 +16,17 @@ ModeldataKO$time = 0:431
 ModeldataWT$time = ModeldataWT$time / 24
 ModeldataKO$time = ModeldataKO$time / 24
 
+
+#####
+#-----------------------------------------------#
+#                   AUC - Naive Tregs
+#-----------------------------------------------#
+
+auc_wt <- trapz(ModeldataWT$time, ModeldataWT$TregNaive)
+auc_ko <- trapz(ModeldataKO$time, ModeldataKO$TregNaive)
+
+#
+test_result <- wilcox.test(ModeldataWT$TregNaive, ModeldataKO$TregNaive)
+
+# Output the AUC values and test results
+list(AUC_WT = auc_wt, AUC_KO = auc_ko, Test_Result = test_result)
